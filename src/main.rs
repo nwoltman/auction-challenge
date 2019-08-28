0f9cbf4f23715fd57084d5b7dbd20b9bf016b26b
Nathan Woltman <nwoltman@outlook.com>
2019-08-28 01:15:33 -0400

    Add basic parallelization
    This improves processing speed by about 54% (measured on ~7 MB of input).

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 mod auction;
 mod auction_config;
 
-use serde::de::{self, Deserializer, SeqAccess, Visitor};
+use serde::de::{Deserializer, SeqAccess, Visitor};
 use serde::ser::{SerializeSeq, Serializer};
 
 use std::fmt;
 use std::io::{self, BufReader, BufWriter};
+use std::sync::mpsc;
+use std::thread;
 
 const CONFIG_PATH: &str = "/auction/config.json";
 
-struct AuctionProcessor<'s, S: SerializeSeq> {
-    config: auction_config::Config,
-    seq_serializer: &'s mut S,
+struct AuctionProcessor {
+    sender: mpsc::Sender<auction::Auction>,
 }
 
-impl<'s, S> Visitor<'s> for AuctionProcessor<'s, S>
-where
-    S: SerializeSeq,
-{
+impl<'s> Visitor<'s> for AuctionProcessor {
     type Value = ();
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -28,32 +26,34 @@ where
     where
         SA: SeqAccess<'s>,
     {
-        // Stream the auctions, processing them and serializing the results as they arrive
+        // Deserialize the auctions and send them to the main thread for processing
         while let Some(auction) = seq.next_element::<auction::Auction>()? {
-            let winning_bids = auction::get_winning_bids(&auction, &self.config);
-            self.seq_serializer
-                .serialize_element(&winning_bids)
-                .map_err(de::Error::custom)?;
+            self.sender.send(auction).unwrap();
         }
-
         Ok(())
     }
 }
 
 fn main() -> serde_json::Result<()> {
-    let reader = BufReader::new(io::stdin());
-    let mut deserializer = serde_json::Deserializer::from_reader(reader);
+    let (sender, receiver) = mpsc::channel();
 
+    // Deserialize the input on a separate thread
+    thread::spawn(move || {
+        let auction_processor = AuctionProcessor { sender };
+        let reader = BufReader::new(io::stdin());
+        let mut deserializer = serde_json::Deserializer::from_reader(reader);
+        deserializer.deserialize_seq(auction_processor).unwrap();
+    });
+
+    let config = auction_config::get_config(CONFIG_PATH);
     let writer = BufWriter::new(io::stdout());
     let mut serializer = serde_json::Serializer::new(writer);
     let mut seq_serializer = serializer.serialize_seq(None)?;
 
-    let auction_processor = AuctionProcessor {
-        config: auction_config::get_config(CONFIG_PATH),
-        seq_serializer: &mut seq_serializer,
-    };
-
-    deserializer.deserialize_seq(auction_processor).unwrap();
+    for auction in receiver.iter() {
+        let winning_bids = auction::get_winning_bids(&auction, &config);
+        seq_serializer.serialize_element(&winning_bids)?;
+    }
 
     seq_serializer.end()
 }
